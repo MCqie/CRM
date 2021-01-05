991f28584b5d46ba0cf62f33cc41bfc1452184b0
MCqie <1051846107@qq.com>
2021-01-05 18:11:59 +0800

    第一个Stm32程序(Clion)

---
+++ eee/cmake-build-debug/CMakeFiles/eee.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eee.dir/main.cpp.obj"
+  "eee.exe"
+  "eee.exe.manifest"
+  "eee.lib"
+  "eee.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
